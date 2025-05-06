315bd73987e4447fb0e4bb55b5fa4242a4109c04
lebluxtv <136271820+lebluxtv@users.noreply.github.com>
2025-05-06 08:52:50 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,17 +1,17 @@
 onmessage = (e) => {
   const data = e.data.data;
-
   let r = 0, g = 0, b = 0;
+
   for (let i = 0; i < data.length; i += 4) {
     r += data[i];
     g += data[i + 1];
     b += data[i + 2];
   }
 
-  const pixels = data.length / 4;
-  r = Math.round(r / pixels);
-  g = Math.round(g / pixels);
-  b = Math.round(b / pixels);
-
-  postMessage({ r, g, b });
+  const count = data.length / 4;
+  postMessage({
+    r: Math.round(r / count),
+    g: Math.round(g / count),
+    b: Math.round(b / count)
+  });
 };
